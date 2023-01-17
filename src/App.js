d7921f9959fb2deff9b3f1326724b168bd6c9b5d
pri65 <priyankasoniii65@gmail.com>
2023-01-17 13:08:55 +0530

    Set routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Routes,Route } from 'react-router-dom'; //we gonna use this to route between our homepage and detail page
+import { Box } from '@mui/material'; //this box component have is simply a div having some shading and colors
 import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 const App = () => {
   return (
-    <div>App</div>
+    //we wrap our entire app inside this box component
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+    </Box>
   )
 }
 
